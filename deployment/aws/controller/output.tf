8984a04b2d6fca248010dd253ec056074c187208
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-09-02 15:40:04 +0200

    output controller security_group and subnet

--- deployment/aws/controller/output.tf
+++ deployment/aws/controller/output.tf
@@ -6,3 +6,11 @@ output "controller_dns" {
 output "key_name" {
   value = aws_key_pair.deployer[0].id
 }
+
+
+output "controller_security_groups" {
+  value = [var.security_group == "" ? aws_security_group.appgate_security_group[0].id : var.security_group]
+}
+output "controller_subnet" {
+  value = aws_instance.appgate_controller.subnet_id
+}
